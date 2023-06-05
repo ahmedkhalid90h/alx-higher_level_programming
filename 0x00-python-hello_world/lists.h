6b3df211fbe8535999d0824bf80eea484bb968a7
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-06-05 23:45:43 +0300

    vfg

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -17,9 +17,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
+
 
 #endif /* LISTS_H */
